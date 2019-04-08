5057605225b778fcef4ca98a95597fd59ee0a0e7
ousmane91 <contact@ousmangueye.fr>
2019-04-08 14:14:08 +0200

    exomaj

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //fonction pour effacer les données des champs du formulaire avec le bouton RAZ
 function resetForm() {
-  var lastname = document.getElementById('lastname').value="";
-  var firstname = document.getElementById('firstname').value="";
-  var city = document.getElementById('city').value="";
+  var lastname = document.getElementById('lastname').value='';
+  var firstname = document.getElementById('firstname').value='';
+  var city = document.getElementById('city').value='';
 }
